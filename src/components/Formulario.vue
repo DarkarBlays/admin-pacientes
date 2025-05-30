daba3f54a0386a93e9d8b7cebaed287fecea4a63
DarkarBlays <juanpamendoza08@gmail.com>
2025-05-30 14:26:29 -0500

    Formulario creado, validacion de campos y componente de alerta

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,30 @@
 <script setup>
+import { reactive } from 'vue';
+import Alerta from './Alerta.vue';
+
+const alerta = reactive({
+    tipo: '',
+    mensaje: ''
+})
+
+const paciente = reactive({
+    nombre: '',
+    propietario: '',
+    email: '',
+    alta: '',
+    sintomas: ''
+});
+
+const validar = () => {
+
+    if ([Object.values(paciente).includes('')]) {
+        alerta.mensaje = 'Todos los campos son obligatorios';
+        alerta.tipo = 'error'
+        return
+    }
+    
+    console.log('Agregando...')
+}
 
 </script>
 
@@ -9,13 +35,50 @@
         <p class="text-lg mt-5 text-center mb-10">Añade Pacientes y <span
                 class="text-indigo-600 font-bold">Administralos</span></p>
 
-        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <Alerta 
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        />
+
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" @submit.prevent="validar">
             <div class="mb-5">
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
 
                 <input id="mascota" type="text" placeholder="Nombre de la Mascota"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="paciente.nombre">
             </div>
+
+            <div class="mb-5">
+                <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+
+                <input id="propietario" type="text" placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="paciente.propietario">
+            </div>
+
+            <div class="mb-5">
+                <label for="email" class="block text-gray-700 uppercase font-bold">Email</label>
+
+                <input id="email" type="email" placeholder="Email del propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="paciente.email">
+            </div>
+
+            <div class="mb-5">
+                <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
+
+                <input id="alta" type="date" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.alta">
+            </div>
+
+            <div class="mb-5">
+                <label for="sintomas" class="block text-gray-700 uppercase font-bold">Sintomas</label>
+
+                <textarea id="sintomas" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                    placeholder="Describe los sintomas"></textarea>
+            </div>
+
+            <input type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                value="Registrar Paciente">
         </form>
     </div>
 </template>
