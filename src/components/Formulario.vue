4bfc9d07d39a1943b3d992322e833375b1a9f5ed
DarkarBlays <juanpamendoza08@gmail.com>
2025-05-30 15:10:13 -0500

    moviento y ajustando el objeto de pacientes en App, para implementarlo mas facil en cualquier componente

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,73 +7,91 @@ const alerta = reactive({
     mensaje: ''
 })
 
-const paciente = reactive({
-    nombre: '',
-    propietario: '',
-    email: '',
-    alta: '',
-    sintomas: ''
-});
+defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas']);
+
+const props = defineProps({
+    nombre: {
+        type: String,
+        required: true
+    },
+    propietario: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    alta: {
+        type: String,
+        required: true
+    },
+    sintomas: {
+        type: String,
+        required: true
+    }
+})
 
 const validar = () => {
 
-    if ([Object.values(paciente).includes('')]) {
+    if ([Object.values(props).includes('')]) {
         alerta.mensaje = 'Todos los campos son obligatorios';
         alerta.tipo = 'error'
         return
     }
-    
+
     console.log('Agregando...')
 }
 
 </script>
 
 <template>
-    <div class="md:w1/2 ">
+    <div class="md:w-1/2 ">
         <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
         <p class="text-lg mt-5 text-center mb-10">Añade Pacientes y <span
                 class="text-indigo-600 font-bold">Administralos</span></p>
 
-        <Alerta 
-            v-if="alerta.mensaje"
-            :alerta="alerta"
-        />
+        <Alerta v-if="alerta.mensaje" :alerta="alerta" />
 
         <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" @submit.prevent="validar">
             <div class="mb-5">
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
 
                 <input id="mascota" type="text" placeholder="Nombre de la Mascota"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="paciente.nombre">
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" :value="nombre"
+                    @input="$emit('update:nombre', $event.target.value)">
             </div>
 
             <div class="mb-5">
                 <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
 
                 <input id="propietario" type="text" placeholder="Nombre del propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="paciente.propietario">
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" :value="propietario"
+                    @input="$emit('update:propietario', $event.target.value)">
             </div>
 
             <div class="mb-5">
                 <label for="email" class="block text-gray-700 uppercase font-bold">Email</label>
 
                 <input id="email" type="email" placeholder="Email del propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" v-model="paciente.email">
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" :value="email"
+                    @input="$emit('update:email', $event.target.value)">
             </div>
 
             <div class="mb-5">
                 <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
 
                 <input id="alta" type="date" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    v-model="paciente.alta">
+                    :value="alta" @input="$emit('update:alta', $event.target.value)">
             </div>
 
             <div class="mb-5">
                 <label for="sintomas" class="block text-gray-700 uppercase font-bold">Sintomas</label>
 
                 <textarea id="sintomas" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
-                    placeholder="Describe los sintomas"></textarea>
+                    placeholder="Describe los sintomas" :value="sintomas"
+                    @input="$emit('update:sintomas', $event.target.value)"></textarea>
             </div>
 
             <input type="submit"
